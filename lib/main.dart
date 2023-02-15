21d62511f01c116a3eb02d6c1894c6b4d4d41dab
Kanav Arora <kanavarora1515@gmail.com>
2023-02-15 09:48:28 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,97 @@
+import './transaction.dart';
+import 'package:flutter/material.dart';
+import './transactEntry.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Expense Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Expense Tracker'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> transactions = [];
+
+  final title = TextEditingController();
+  final amount = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Card(
+            elevation: 5,
+            color: Colors.blue,
+            child: Container(
+              width: double.infinity,
+              child: const Text("Chart Placeholder"),
+            ),
+          ),
+          Card(
+            elevation: 5,
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              child: Column(children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(labelText: 'Title'),
+                  controller: title,
+                ),
+                TextField(
+                  decoration: InputDecoration(labelText: 'Amount'),
+                  controller: amount,
+                )
+              ]),
+            ),
+          ),
+          Column(
+            children: transactions.map((tx) {
+              return TransactEntry(tx.amount, tx.title, tx.date);
+            }).toList(),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              String t = title.text.toString();
+              String a = amount.text.toString();
+              if (t.isNotEmpty && a.isNotEmpty) {
+                var ad = double.parse(a);
+                title.clear();
+                amount.clear();
+                transactions.insert(
+                    0, Transaction(title: t, amount: ad, date: DateTime.now()));
+              }
+              print("Transaction added");
+            });
+          },
+          child: const Icon(Icons.add)),
+    );
+  }
+}
---
+++ lib/transactEntry.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class TransactEntry extends StatelessWidget {
+  final double amount;
+  final String title;
+  final DateTime date;
+  final DateFormat formatter = DateFormat.yMMMd();
+  TransactEntry(this.amount, this.title, this.date, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 4,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              "\$$amount",
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Text(title, style: const TextStyle(fontWeight: FontWeight.bold)),
+            Text(formatter.format(date),
+                style: const TextStyle(fontWeight: FontWeight.bold)),
+          ],
+        ),
+      ),
+    );
+  }
+}
